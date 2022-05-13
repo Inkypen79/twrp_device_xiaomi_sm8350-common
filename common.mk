ca04fe7c103cbe97f4fc6baeeda18c3d40321e5d
Nicholas Magill <nmagill1979@gmail.com>
2022-05-13 13:06:28 +0100

    sm8350-common: Correct fuel gauge driver name
    Change-Id: Ibbcd6839b2ebb056f5ec113fc1d660245aa41970

--- common.mk
+++ common.mk
@@ -561,7 +561,7 @@ PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
-TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k2.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger.ko"
+TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k2.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger_main.ko"
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.vibrator-ndk_platform \
