e36a213bffbf87e77a7006ef15599ddeeaa2146e
Nicholas Magill <nmagill1979@gmail.com>
2022-10-18 16:02:01 +0100

    sm8350-common: Add odin A12 fuel gauge driver name

--- common.mk
+++ common.mk
@@ -561,7 +561,7 @@ PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
-TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k8.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger_main.ko"
+TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k8.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger_main.ko qti_battery_charger_main_odin.ko"
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
